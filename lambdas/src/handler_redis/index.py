d0e6bb54451316a31c1b1cc2989f934aa236b0f8
order&chaos <sean@orderandchaoscreative.com>
2022-12-10 02:25:25 +0000

    here

--- lambdas/src/handler_redis/index.py
+++ lambdas/src/handler_redis/index.py
@@ -1,30 +1,30 @@
 import os
+import json
 import redis
+import numpy as np
 from nearpy import Engine
 from nearpy.hashes import RandomBinaryProjections
 from nearpy.filters import NearestFilter
 from nearpy.storage import RedisStorage
-import numpy as np
+
+cache_host = os.environ["CACHE_HOST"]
+cache_port = os.environ["CACHE_PORT"]
+r = redis.Redis(host=cache_host, port=cache_port, decode_responses=True)
+storage = RedisStorage(r)
+rbp = RandomBinaryProjections('rbp', 10)
+dimension = 100
+engine = Engine(dimension, lshashes=[rbp], storage=storage)
+# engine.clean_all_buckets()
+
+for index in range(100000):
+    v = np.random.randn(dimension)
+    engine.store_vector(v, 'data_%d' % index)
 
 def handler(event, context):
-    # Get the cache host and port from the environment variables
-    cache_host = os.environ["CACHE_HOST"]
-    cache_port = os.environ["CACHE_PORT"]
-
-    # Set up NearPy engine with Redis storage
-    r = redis.Redis(host=cache_host, port=cache_port, decode_responses=True)
-    storage = RedisStorage(r)
-    rbp = RandomBinaryProjections('rbp', 10)
-    dimension = 100
-    engine = Engine(dimension, lshashes=[rbp], storage=storage)
-
-    for index in range(10000):
-        v = np.random.randn(dimension)
-        engine.store_vector(v, 'data_%d' % index)
-
     query = np.random.randn(dimension)
-
     nearest = engine.neighbours(query)
-    print(nearest)
 
-    return "Success"
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"nearest": str(nearest)})
+    }
